29f718b74a1464d50fa6a0772ccdeb357ff3a9d4
Sina Iman <sina@kryptnostic.com>
2014-09-19 00:23:43 -0700

    removed hazelcast dependency from MetricsPod and added an InMemoryConfigurationServicePod for when Hazelcast isn't required

--- src/main/java/com/geekbeast/rhizome/configuration/service/AbstractYamlConfigurationService.java
+++ src/main/java/com/geekbeast/rhizome/configuration/service/AbstractYamlConfigurationService.java
@@ -18,59 +18,58 @@ import com.google.common.base.Preconditions;
 import com.google.common.eventbus.AsyncEventBus;
 
 /**
- * Abstract class for loading YAML format configuration classes that handles low level resource reading, validation, and update publishing.
+ * Abstract class for loading YAML format configuration classes that handles low level resource reading, validation, and
+ * update publishing.
+ * 
  * @author Matthew Tamayo-Rios
  */
-//TODO: Add hibernate validation
+// TODO: Add hibernate validation
 public abstract class AbstractYamlConfigurationService implements ConfigurationService {
     protected final static ObjectMapper mapper = ObjectMapperRegistry.getYamlMapper();
-    
-    protected final Logger logger = LoggerFactory.getLogger( getClass() );
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
     protected final AsyncEventBus configurationEvents;
-    
-    public AbstractYamlConfigurationService(
-            AsyncEventBus configurationEvents 
-            ) {
+
+    public AbstractYamlConfigurationService(AsyncEventBus configurationEvents) {
         this.configurationEvents = configurationEvents;
     }
 
     @Override
-    public <T extends Configuration> T getConfiguration( Class<T> clazz ) throws IOException {
-        Preconditions.checkNotNull( clazz , "Requested configuration class cannot be null." );
-        ConfigurationKey key = ConfigurationService.StaticLoader.getConfigurationKey( clazz );
-        Preconditions.checkState( 
-                key!=null && StringUtils.isBlank( key.getUri() )  , 
-                "Configuration id for class " + clazz.getName() + " cannot be blank or null" );
-        
+    public <T extends Configuration> T getConfiguration(Class<T> clazz) throws IOException {
+        Preconditions.checkNotNull(clazz, "Requested configuration class cannot be null.");
+        ConfigurationKey key = ConfigurationService.StaticLoader.getConfigurationKey(clazz);
+        Preconditions.checkState(key != null && StringUtils.isBlank(key.getUri()), "Configuration id for class "
+                + clazz.getName() + " cannot be blank or null");
+
         try {
-            return mapper.readValue( 
-                   Preconditions.checkNotNull( fetchConfiguration( key ) , "Configuration cannot be null" ) , 
-                   clazz );
-        } catch (JsonParseException|JsonMappingException e) {
-            logger.error("Invalid YAML configuration file for class " + clazz.getName() );
+            return mapper.readValue(
+                    Preconditions.checkNotNull(fetchConfiguration(key), "Configuration cannot be null"), clazz);
+        } catch (JsonParseException | JsonMappingException e) {
+            logger.error("Invalid YAML configuration file for class " + clazz.getName());
             return null;
-        } 
+        }
     }
 
     @Override
     public <T extends Configuration> void setConfiguration(T configuration) {
         try {
-            persistConfiguration( configuration.getKey() , mapper.writeValueAsString( configuration ) );
-            post( configuration );
-        } catch(IOException e ) {
-            logger.error( "Failed to persist configuration {}",configuration);
+            persistConfiguration(configuration.getKey(), mapper.writeValueAsString(configuration));
+            post(configuration);
+        } catch (IOException e) {
+            logger.error("Failed to persist configuration {}", configuration);
         }
     }
 
     @Override
     public void subscribe(Object subscriber) {
-        configurationEvents.register( subscriber );
+        configurationEvents.register(subscriber);
     }
-    
-    protected void post( Configuration configuration ) {
-        configurationEvents.post( configuration );
+
+    protected void post(Configuration configuration) {
+        configurationEvents.post(configuration);
     }
-    
-    protected abstract @Nullable String fetchConfiguration( ConfigurationKey key );
-    protected abstract void persistConfiguration( ConfigurationKey key , String configurationYaml );
+
+    protected abstract @Nullable String fetchConfiguration(ConfigurationKey key);
+
+    protected abstract void persistConfiguration(ConfigurationKey key, String configurationYaml);
 }
---
+++ src/main/java/com/geekbeast/rhizome/pods/InMemoryConfigurationServicePod.java
@@ -0,0 +1,38 @@
+package com.geekbeast.rhizome.pods;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import jersey.repackaged.com.google.common.collect.Maps;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.geekbeast.rhizome.configuration.ConfigurationKey;
+import com.geekbeast.rhizome.configuration.service.AbstractYamlConfigurationService;
+import com.geekbeast.rhizome.configuration.service.ConfigurationService;
+import com.google.common.eventbus.AsyncEventBus;
+
+@Configuration
+public class InMemoryConfigurationServicePod {
+    @Inject
+    AsyncEventBus eventBus;
+    
+    @Bean
+    public ConfigurationService configurationService() {
+        return new AbstractYamlConfigurationService(eventBus) {
+            private Map<ConfigurationKey, String> map = Maps.newHashMap();
+
+            @Override
+            protected void persistConfiguration(ConfigurationKey key, String configurationYaml) {
+                map.put(key,  configurationYaml);
+            }
+
+            @Override
+            protected String fetchConfiguration(ConfigurationKey key) {
+                return map.get(key);
+            }
+        };
+    }
+}
