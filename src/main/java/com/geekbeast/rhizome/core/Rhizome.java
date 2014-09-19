6fffabff15ebc1e8e9edddc62f40fb21b20858a2
Sina Iman <sina@kryptnostic.com>
2014-09-19 00:07:43 -0700

    make Rhizome require instantiation so that initialize can be not static and thus overridable

--- src/main/java/com/geekbeast/rhizome/core/Rhizome.java
+++ src/main/java/com/geekbeast/rhizome/core/Rhizome.java
@@ -42,140 +42,154 @@ import com.google.common.collect.Sets;
 import com.hazelcast.web.SessionListener;
 import com.hazelcast.web.WebFilter;
 
+
+/**
+ * Note: if using jetty, jetty creates an instance of this class with a no-arg constructor in order to call onStartup
+ * TODO: break out WebApplicationInitializer's onStartup to a different class because of Jetty issue
+ *
+ */
 public class Rhizome implements WebApplicationInitializer {
     private static final String HAZELCAST_SESSION_FILTER_NAME = "hazelcastSessionFilter";
     private static final String GZIP_FILTER_NAME = "GzipFilter";
     private static final String CONTEXT_CONFIG_LOCATION_PARAMETER_NAME = "contextConfigLocation";
     private static final String CONTEXT_PARAMETER_NAME = "contextClass";
     private static final String MIME_TYPES_PARAM = "mimeTypes";
-    protected static final AnnotationConfigWebApplicationContext rhizomeContext = new AnnotationConfigWebApplicationContext();
+    protected final static AnnotationConfigWebApplicationContext rhizomeContext = new AnnotationConfigWebApplicationContext();
+    private static boolean isInitialized = false;
 
-    static {
+    public Rhizome() {
         initialize();
     }
-    
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        servletContext.addListener( new SessionListener() );
-        
+        servletContext.addListener(new SessionListener());
+
         /*
-         * We have the luxury of being able to access the RhizomeConfiguration from the rhizomeContext.
-         * This allows us to conditionally enabled session clustering among other things.
+         * We have the luxury of being able to access the RhizomeConfiguration from the rhizomeContext. This allows us
+         * to conditionally enabled session clustering among other things.
          */
-        
-        JettyConfiguration jettyConfig   = rhizomeContext.getBean( JettyConfiguration.class );
-        RhizomeConfiguration configuration = rhizomeContext.getBean( RhizomeConfiguration.class );
-        if( configuration.isSessionClusteringEnabled() ) {
-            servletContext
-                .addFilter( HAZELCAST_SESSION_FILTER_NAME , rhizomeContext.getBean( WebFilter.class ) )
-                .addMappingForUrlPatterns(
-                    Sets.newEnumSet( ImmutableSet.of( DispatcherType.FORWARD  , DispatcherType.REQUEST , DispatcherType.REQUEST ) , DispatcherType.class ) , 
-                    false , 
-                    "/*");
+
+        JettyConfiguration jettyConfig = rhizomeContext.getBean(JettyConfiguration.class);
+        RhizomeConfiguration configuration = rhizomeContext.getBean(RhizomeConfiguration.class);
+        if (configuration.isSessionClusteringEnabled()) {
+            servletContext.addFilter(HAZELCAST_SESSION_FILTER_NAME, rhizomeContext.getBean(WebFilter.class))
+                    .addMappingForUrlPatterns(
+                            Sets.newEnumSet(ImmutableSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+                                    DispatcherType.REQUEST), DispatcherType.class), false, "/*");
         }
-        
+
         Optional<GzipConfiguration> gzipConfig = jettyConfig.getGzipConfiguration();
-        if( gzipConfig.isPresent() && gzipConfig.get().isGzipEnabled() ) {
-            FilterRegistration.Dynamic gzipFilter = servletContext.addFilter( GZIP_FILTER_NAME , new GzipFilter() );
-            gzipFilter.addMappingForUrlPatterns( null, false, "/*");
-            gzipFilter.setInitParameter( MIME_TYPES_PARAM , Joiner.on(",").skipNulls().join( gzipConfig.get().getGzipContentTypes() )  );
+        if (gzipConfig.isPresent() && gzipConfig.get().isGzipEnabled()) {
+            FilterRegistration.Dynamic gzipFilter = servletContext.addFilter(GZIP_FILTER_NAME, new GzipFilter());
+            gzipFilter.addMappingForUrlPatterns(null, false, "/*");
+            gzipFilter.setInitParameter(MIME_TYPES_PARAM,
+                    Joiner.on(",").skipNulls().join(gzipConfig.get().getGzipContentTypes()));
         }
-        
-        //Prevent jersey-spring3 from trying to initialize a spring application context.
-//        servletContext.setInitParameter( CONTEXT_CONFIG_LOCATION_PARAMETER_NAME , "" );
-        servletContext.addListener( new ContextLoaderListener( rhizomeContext ) );
-        servletContext.addListener( new RequestContextListener() );
-        
-        //Register the health check registry.
-        servletContext.setAttribute( HealthCheckServlet.HEALTH_CHECK_REGISTRY , rhizomeContext.getBean( "healthCheckRegistry" , HealthCheckRegistry.class ) );
-        servletContext.setAttribute( MetricsServlet.METRICS_REGISTRY , rhizomeContext.getBean( "serverMetricRegistry" , MetricRegistry.class ) );
-        
+
+        // Prevent jersey-spring3 from trying to initialize a spring application context.
+        // servletContext.setInitParameter( CONTEXT_CONFIG_LOCATION_PARAMETER_NAME , "" );
+        servletContext.addListener(new ContextLoaderListener(rhizomeContext));
+        servletContext.addListener(new RequestContextListener());
+
+        // Register the health check registry.
+        servletContext.setAttribute(HealthCheckServlet.HEALTH_CHECK_REGISTRY,
+                rhizomeContext.getBean("healthCheckRegistry", HealthCheckRegistry.class));
+        servletContext.setAttribute(MetricsServlet.METRICS_REGISTRY,
+                rhizomeContext.getBean("serverMetricRegistry", MetricRegistry.class));
+
         /*
          * 
          */
-        
-        ServletRegistration.Dynamic adminServlet = servletContext.addServlet( "admin" , AdminServlet.class );
-        adminServlet.setLoadOnStartup( 1 );
+
+        ServletRegistration.Dynamic adminServlet = servletContext.addServlet("admin", AdminServlet.class);
+        adminServlet.setLoadOnStartup(1);
         adminServlet.addMapping("/admin/*");
-        adminServlet.setInitParameter("show-jvm-metrics", "true" );
-        
+        adminServlet.setInitParameter("show-jvm-metrics", "true");
+
         /*
-         * Jersey Servlet
-         * For lovers of the JAX-RS standard.  
+         * Jersey Servlet For lovers of the JAX-RS standard.
          */
-        
-        ServletRegistration.Dynamic jerseyDispatcher = 
-            servletContext.addServlet( "defaultJerseyServlet" , new ServletContainer( ) );
-        jerseyDispatcher.setInitParameter( "javax.ws.rs.Application", RhizomeApplication.class.getName() );
+
+        ServletRegistration.Dynamic jerseyDispatcher = servletContext.addServlet("defaultJerseyServlet",
+                new ServletContainer());
+        jerseyDispatcher.setInitParameter("javax.ws.rs.Application", RhizomeApplication.class.getName());
         jerseyDispatcher.setLoadOnStartup(1);
         jerseyDispatcher.addMapping("/health/*");
-        
+
         /*
-         * Atmosphere Servlet  
+         * Atmosphere Servlet
          */
-        
-        //TODO: Add support for atmosphere servlet.
-        
+
+        // TODO: Add support for atmosphere servlet.
+
         /*
          * Default Servlet
          */
-        ServletRegistration.Dynamic defaultServlet = servletContext.addServlet( "default" , new DefaultServlet() );
-        defaultServlet.addMapping( new String[]{ "/*" } );
+        ServletRegistration.Dynamic defaultServlet = servletContext.addServlet("default", new DefaultServlet());
+        defaultServlet.addMapping(new String[] { "/*" });
         defaultServlet.setLoadOnStartup(1);
-        
-        registerDispatcherServlets( servletContext );
+
+        registerDispatcherServlets(servletContext);
     }
-    
-    private void registerDispatcherServlets( ServletContext servletContext ) {
-        Map<String,DispatcherServletConfiguration> dispatcherServletsConfigs = rhizomeContext.getBeansOfType( DispatcherServletConfiguration.class , false , true );
-        for( Entry<String, DispatcherServletConfiguration> configPair : dispatcherServletsConfigs.entrySet() ) {
+
+    private void registerDispatcherServlets(ServletContext servletContext) {
+        Map<String, DispatcherServletConfiguration> dispatcherServletsConfigs = rhizomeContext.getBeansOfType(
+                DispatcherServletConfiguration.class, false, true);
+        for (Entry<String, DispatcherServletConfiguration> configPair : dispatcherServletsConfigs.entrySet()) {
             DispatcherServletConfiguration configuration = configPair.getValue();
             AnnotationConfigWebApplicationContext dispatchServletContext = new AnnotationConfigWebApplicationContext();
-            dispatchServletContext.setParent( rhizomeContext );
-            dispatchServletContext.register( configuration.getPods().toArray( new Class<?>[0] ) );
-            ServletRegistration.Dynamic dispatcher = servletContext.addServlet( configuration.getServletName() , new DispatcherServlet( dispatchServletContext ) );
-            if( configuration.getLoadOnStartup().isPresent() ) {
-                dispatcher.setLoadOnStartup( configuration.getLoadOnStartup().get() );
+            dispatchServletContext.setParent(rhizomeContext);
+            dispatchServletContext.register(configuration.getPods().toArray(new Class<?>[0]));
+            ServletRegistration.Dynamic dispatcher = servletContext.addServlet(configuration.getServletName(),
+                    new DispatcherServlet(dispatchServletContext));
+            if (configuration.getLoadOnStartup().isPresent()) {
+                dispatcher.setLoadOnStartup(configuration.getLoadOnStartup().get());
             }
-            dispatcher.addMapping( configuration.getMappings() );
+            dispatcher.addMapping(configuration.getMappings());
         }
     }
-    
-    public static AnnotationConfigWebApplicationContext getContext() { 
+
+    public AnnotationConfigWebApplicationContext getContext() {
         return rhizomeContext;
     }
-    
-    public static <T> T harvest( Class<T> clazz ) {
-        return rhizomeContext.getBean( clazz );
+
+    public <T> T harvest(Class<T> clazz) {
+        return rhizomeContext.getBean(clazz);
     }
-    
-    public static void intercrop( Class<?> ... pods ) {
-        rhizomeContext.register( pods );
+
+    public void intercrop(Class<?>... pods) {
+        rhizomeContext.register(pods);
     }
-    
-    public static void sprout() throws Exception {
+
+    public void sprout() throws Exception {
         rhizomeContext.refresh();
-        for( Loam loam : rhizomeContext.getBeansOfType( Loam.class ).values() ) {
+        for (Loam loam : rhizomeContext.getBeansOfType(Loam.class).values()) {
             loam.start();
         }
     }
-    
-    public static void wilt() throws BeansException, Exception {
-        for( Loam loam : rhizomeContext.getBeansOfType( Loam.class ).values() ) {
+
+    public void wilt() throws BeansException, Exception {
+        for (Loam loam : rhizomeContext.getBeansOfType(Loam.class).values()) {
             loam.stop();
         }
     }
-    
+
     /**
-     * This method should be overridden if any of the built-in defaults are not desired. 
-     * To add additional configurations beyond the built in defaults, {@code intercrop(...)} 
-     * should be called to register @Configuration bootstrap beans. 
+     * This method should be overridden if any of the built-in defaults are not desired. To add additional
+     * configurations beyond the built in defaults, {@code intercrop(...)} should be called to register @Configuration
+     * bootstrap beans.
      */
-    protected static void initialize() {
-        rhizomeContext.register( ConfigurationPod.class );
-        rhizomeContext.register( MetricsPod.class );
-        rhizomeContext.register( AsyncPod.class );
-        rhizomeContext.register( HazelcastPod.class );
-        rhizomeContext.register( ServletContainerPod.class );
+    protected void initialize() {
+        synchronized (rhizomeContext) {
+            if (!isInitialized) {
+                rhizomeContext.register(ConfigurationPod.class);
+                rhizomeContext.register(MetricsPod.class);
+                rhizomeContext.register(AsyncPod.class);
+                rhizomeContext.register(HazelcastPod.class);
+                rhizomeContext.register(ServletContainerPod.class);
+                isInitialized = true;
+            }
+        }
     }
 }
