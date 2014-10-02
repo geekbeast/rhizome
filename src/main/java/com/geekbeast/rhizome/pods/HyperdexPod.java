08ccd6f7b1a1f076e3a81552b9e3c308b9868423
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-01 22:40:18 -0700

    Made sure hyperdex pre-configurer was always called.

--- src/main/java/com/geekbeast/rhizome/pods/HyperdexPod.java
+++ src/main/java/com/geekbeast/rhizome/pods/HyperdexPod.java
@@ -1,7 +1,5 @@
 package com.geekbeast.rhizome.pods;
 
-import javax.inject.Inject;
-
 import org.hyperdex.client.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,14 +8,18 @@ import org.springframework.context.annotation.Configuration;
 
 import com.geekbeast.rhizome.configuration.RhizomeConfiguration;
 import com.geekbeast.rhizome.configuration.hyperdex.HyperdexConfiguration;
+import com.geekbeast.rhizome.configuration.hyperdex.HyperdexPreconfigurer;
 
 @Configuration
 public class HyperdexPod {
     private static final Logger logger = LoggerFactory.getLogger( HyperdexPod.class );
     
-    @Inject 
-    private RhizomeConfiguration configuration;
+    private static final RhizomeConfiguration configuration = ConfigurationPod.getRhizomeConfiguration();
     
+    static {
+        HyperdexPreconfigurer.configure();
+    }
+
     @Bean
     public HyperdexConfiguration hyperdexConfiguration() {
         return configuration.getHyperdexConfiguration().get();
