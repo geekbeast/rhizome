645ca2d944c5feb2259897efe382f75bf97094dd
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-01 22:40:18 -0700

    Made sure hyperdex pre-configurer was always called.

--- src/main/java/com/kryptnostic/rhizome/mapstores/SimpleHyperdexMapper.java
+++ src/main/java/com/kryptnostic/rhizome/mapstores/SimpleHyperdexMapper.java
@@ -13,20 +13,20 @@ import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.google.common.collect.ImmutableMap;
 
-public class SimpleHyperdexPacker<V> implements HyperdexMapper<V> {
-    private static final Logger logger = LoggerFactory.getLogger( SimpleHyperdexPacker.class );
+public class SimpleHyperdexMapper<V> implements HyperdexMapper<V> {
+    private static final Logger logger = LoggerFactory.getLogger( SimpleHyperdexMapper.class );
     private static final String DATA_ATTRIBUTE = "data";
     
     private final ObjectMapper mapper;
     private final Class<V> valueClass;
     
-    public SimpleHyperdexPacker( Class<V> valueClass ) {
+    public SimpleHyperdexMapper( Class<V> valueClass ) {
         this( valueClass, new ObjectMapper() );
         mapper.registerModule( new GuavaModule() );
         mapper.registerModule( new AfterburnerModule() );
     }
     
-    public SimpleHyperdexPacker( Class<V> valueClass, ObjectMapper mapper ) {
+    public SimpleHyperdexMapper( Class<V> valueClass, ObjectMapper mapper ) {
         this.mapper = mapper;
         this.valueClass = valueClass;
     }
@@ -44,7 +44,7 @@ public class SimpleHyperdexPacker<V> implements HyperdexMapper<V> {
     @Override
     public V fromHyperdexMap(Map<String, Object> hyperdexMap) throws HyperdexMappingException {
         try {
-            return mapper.readValue( ((ByteString) hyperdexMap.get( DATA_ATTRIBUTE ) ).toString() , valueClass );
+            return hyperdexMap==null ? null : mapper.readValue( ((ByteString) hyperdexMap.get( DATA_ATTRIBUTE ) ).toString() , valueClass );
         } catch (IOException e) {
             logger.error( "Unable to unmarshall data from hyperdex map {}" , hyperdexMap , e );
             throw new HyperdexMappingException( "Error unmarshalling data from hyperdex map." );
