1a61da043692e58bba920553946377694e14112c
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-18 22:59:35 -0700

    Removed space from base hyperdex configuration

--- src/main/java/com/geekbeast/rhizome/configuration/hyperdex/HyperdexConfiguration.java
+++ src/main/java/com/geekbeast/rhizome/configuration/hyperdex/HyperdexConfiguration.java
@@ -8,7 +8,6 @@ import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 
 public class HyperdexConfiguration {
-    private static final String HYPERDEX_SPACE_PROPERTY = "space";
     private static final String HYPERDEX_PORT_PROPERTY = "port";
     private static final String HYPERDEX_COORDINATORS_PROPERTY = "coordinators";
     private static final String HYPERDEX_NATIVE_BINARY_FOLDER_PROPERTY = "native-bin-path";
@@ -27,7 +26,6 @@ public class HyperdexConfiguration {
     @JsonCreator
     public HyperdexConfiguration(  
             @JsonProperty( HYPERDEX_COORDINATORS_PROPERTY ) Optional<List<String>> coordinators ,
-            @JsonProperty( HYPERDEX_SPACE_PROPERTY ) Optional<String> keyspace ,
             @JsonProperty( HYPERDEX_PORT_PROPERTY ) Optional<Integer> port ,
             @JsonProperty( HYPERDEX_NATIVE_BINARY_FOLDER_PROPERTY ) Optional<String> nativeBinPath ) {
         
@@ -40,11 +38,6 @@ public class HyperdexConfiguration {
     @JsonProperty( HYPERDEX_COORDINATORS_PROPERTY ) 
     public List<String> getCoordinators() {
         return coordinators;
-    }
-    
-    @JsonProperty( HYPERDEX_SPACE_PROPERTY )
-    public String getSpace() {
-        return keyspace;
     }
     
     @JsonProperty( HYPERDEX_PORT_PROPERTY )
