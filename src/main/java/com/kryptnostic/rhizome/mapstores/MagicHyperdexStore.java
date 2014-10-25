cf92731ca162b6fcdf3f101b8585c47da9a35ce3
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-25 02:30:50 -0700

    Update hyperdex map store.

--- src/main/java/com/kryptnostic/rhizome/mapstores/MagicHyperdexStore.java
+++ src/main/java/com/kryptnostic/rhizome/mapstores/MagicHyperdexStore.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.kryptnostic.rhizome.hyperdex.mappers.keys.JacksonHyperdexKeyMapper;
 import com.kryptnostic.rhizome.hyperdex.mappers.values.TypeReferenceHyperdexMapper;
 
-public class MagicHyperdexStore<K,V> extends BaseHyperdexJacksonKeyValueMapStore<K, V>{
+public class MagicHyperdexStore<K, V> extends BaseHyperdexJacksonKeyValueMapStore<K, V> {
 
     public MagicHyperdexStore( String space, Client client, TypeReference<V> reference ) {
         super( space, client, new JacksonHyperdexKeyMapper<K>(), new TypeReferenceHyperdexMapper<V>( reference ) );
