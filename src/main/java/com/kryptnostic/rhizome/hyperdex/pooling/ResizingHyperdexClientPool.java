6b5bc336732ed0355fa4085c9183f7e98f9c77bd
Matthew Tamayo <matthew@kryptnostic.com>
2015-03-11 03:50:34 -0700

    Improvements to hyperdex pool.

--- src/main/java/com/kryptnostic/rhizome/hyperdex/pooling/ResizingHyperdexClientPool.java
+++ src/main/java/com/kryptnostic/rhizome/hyperdex/pooling/ResizingHyperdexClientPool.java
@@ -1,9 +1,8 @@
 package com.kryptnostic.rhizome.hyperdex.pooling;
 
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
-
-import jersey.repackaged.com.google.common.collect.Sets;
+import java.util.concurrent.TimeUnit;
 
 import org.hyperdex.client.Client;
 import org.hyperdex.client.HyperDexClientException;
@@ -13,17 +12,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import com.geekbeast.rhizome.configuration.hyperdex.HyperdexConfiguration;
 import com.google.common.base.Optional;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Queues;
 
 public class ResizingHyperdexClientPool implements HyperdexClientPool {
-    private static final Logger                 logger                  = LoggerFactory
-                                                                                .getLogger( ResizingHyperdexClientPool.class );
-    private static final String                 HEALTH_CHECK_KEY        = "hyperdex-client-pool";
-    private static final String                 HEALTH_CHECK_DATA_FIELD = "data";
-    private static final String                 HEALTH_CHECK_VALUE      = "hyperdex-client-pool-is-good";
+    private static final Logger           logger                  = LoggerFactory
+                                                                          .getLogger( ResizingHyperdexClientPool.class );
+    private static final String           HEALTH_CHECK_KEY        = "hyperdex-client-pool";
+    private static final String           HEALTH_CHECK_DATA_FIELD = "data";
+    private static final String           HEALTH_CHECK_VALUE      = "hyperdex-client-pool-is-good";
 
-    private final ConcurrentLinkedQueue<Client> clients                 = Queues.newConcurrentLinkedQueue();
-    private final HyperdexConfiguration         hyperdexConfiguration;
+    private ConcurrentLinkedQueue<Client> clients                 = Queues.newConcurrentLinkedQueue();
+    private final HyperdexConfiguration   hyperdexConfiguration;
 
     public ResizingHyperdexClientPool( HyperdexConfiguration hyperdexConfiguration ) {
         this.hyperdexConfiguration = hyperdexConfiguration;
@@ -66,16 +67,18 @@ public class ResizingHyperdexClientPool implements HyperdexClientPool {
     @Scheduled(
         fixedRate = 5 * 60000 )
     public void ping() {
-        Client c = acquire();
-        Set<Client> stopSet = Sets.newHashSet();
+        ConcurrentLinkedQueue<Client> oldClients = clients;
+        clients = Queues.newConcurrentLinkedQueue();
 
-        while ( !stopSet.contains( c ) ) {
+        logger.info( "Starting health check of client pool ( size = {} ).", oldClients.size() );
+        Stopwatch w = Stopwatch.createStarted();
+
+        for ( Client c : oldClients ) {
             if ( isClientHealthy( c ) ) {
                 release( c );
-                stopSet.add( c );
             }
-            c = acquire();
         }
+        logger.info( "Health check of client pool took {} ms ", w.elapsed( TimeUnit.MILLISECONDS ) );
     }
 
     public int available() {
@@ -84,9 +87,20 @@ public class ResizingHyperdexClientPool implements HyperdexClientPool {
 
     private boolean isClientHealthy( Client c ) {
         try {
-            return c.get( hyperdexConfiguration.getHealthCheckKeyspace().get(), HEALTH_CHECK_KEY )
-                    .get( HEALTH_CHECK_DATA_FIELD ).toString().equals( HEALTH_CHECK_VALUE );
+            Map<String, Object> result = c.get( hyperdexConfiguration.getHealthCheckKeyspace().get(), HEALTH_CHECK_KEY );
+            if ( result != null && result.containsKey( HEALTH_CHECK_DATA_FIELD ) ) {
+                return result.get( HEALTH_CHECK_DATA_FIELD ).toString().equals( HEALTH_CHECK_VALUE );
+            } else {
+                // Attempt to save value
+                c.put(
+                        hyperdexConfiguration.getHealthCheckKeyspace().get(),
+                        HEALTH_CHECK_KEY,
+                        ImmutableMap.of( HEALTH_CHECK_DATA_FIELD, HEALTH_CHECK_VALUE ) );
+                release( c );
+                return true;
+            }
         } catch ( HyperDexClientException e ) {
+            logger.info( "Health check failed for client." );
             return false;
         }
     }
