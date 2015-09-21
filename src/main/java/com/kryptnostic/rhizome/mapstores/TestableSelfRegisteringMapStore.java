b580260fa067adc226daf480c7a4746c18cc2b25
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-09-21 11:58:22 -0700

    Added exception throwing.

--- src/main/java/com/kryptnostic/rhizome/mapstores/TestableSelfRegisteringMapStore.java
+++ src/main/java/com/kryptnostic/rhizome/mapstores/TestableSelfRegisteringMapStore.java
@@ -3,5 +3,5 @@ package com.kryptnostic.rhizome.mapstores;
 
 public interface TestableSelfRegisteringMapStore<K, V> extends SelfRegisteringMapStore<K,V> {
     K generateTestKey();
-    V generateTestValue();
+    V generateTestValue() throws Exception;
 }
