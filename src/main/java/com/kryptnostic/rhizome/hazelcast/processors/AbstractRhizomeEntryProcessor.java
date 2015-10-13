2cc0ee8babdd350c28b0c3aadde40e1d34b8cbdd
Drew <drew@kryptnostic.com>
2015-10-12 21:01:43 -0700

    add a custom entry processor base to get around a nasty effect of AbstractEntryProcessors backup processor

---
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/processors/AbstractRhizomeEntryProcessor.java
@@ -0,0 +1,36 @@
+package com.kryptnostic.rhizome.hazelcast.processors;
+
+import java.util.Map.Entry;
+
+import com.hazelcast.map.EntryBackupProcessor;
+import com.hazelcast.map.EntryProcessor;
+
+public abstract class AbstractRhizomeEntryProcessor<K, V> implements EntryProcessor<K, V>, EntryBackupProcessor< K, V> {
+    private static final long                serialVersionUID = 5060655249179605949L;
+    private final boolean applyOnBackup;
+
+    /**
+     * Creates an EntryProcessor that applies the {@link #process(java.util.Map.Entry)} to primary and backups.
+     */
+    public AbstractRhizomeEntryProcessor() {
+        this( true );
+    }
+
+    public AbstractRhizomeEntryProcessor( boolean applyOnBackup ) {
+        this.applyOnBackup = applyOnBackup;
+    }
+
+    @Override
+    public final EntryBackupProcessor<K, V> getBackupProcessor() {
+        if (!applyOnBackup){
+            return null;
+        }
+        return this;
+    }
+
+    @Override
+    public void processBackup( Entry<K, V> entry ) {
+        process( entry );
+    }
+
+}
