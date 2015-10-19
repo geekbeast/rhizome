8fe71b3e1b054da45b3d346c10fc46a4f7d5d43c
Drew <drew@kryptnostic.com>
2015-10-18 22:14:05 -0700

    use fast uuid set serialization

--- src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/SetStreamSerializers.java
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/SetStreamSerializers.java
@@ -2,11 +2,14 @@ package com.kryptnostic.rhizome.hazelcast.serializers;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.UUID;
 
 import jersey.repackaged.com.google.common.collect.Sets;
 
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.kryptnostic.rhizome.hazelcast.objects.OrderedUUIDSet;
+import com.kryptnostic.rhizome.hazelcast.objects.UUIDSet;
 
 public class SetStreamSerializers {
 
@@ -15,6 +18,40 @@ public class SetStreamSerializers {
         for( T elem :  elements ) {
             c.accept( elem );
         }
+    }
+
+    public static void fastUUIDSetSerialize( ObjectDataOutput out, Set<UUID> object ) throws IOException{
+        long[] least = new long[object.size()];
+        long[] most = new long[object.size()];
+        int i = 0;
+        for ( UUID uuid : object ) {
+            least[i] = uuid.getLeastSignificantBits();
+            most[i] = uuid.getMostSignificantBits();
+            i++;
+        }
+        out.writeInt( i );
+        out.writeLongArray( least );
+        out.writeLongArray( most );
+    }
+    public static OrderedUUIDSet fastOrderedUUIDSetDeserialize( ObjectDataInput in ) throws IOException{
+        int size = in.readInt();
+        OrderedUUIDSet set = new OrderedUUIDSet( size );
+        return (OrderedUUIDSet) processEntries( set, in );
+    }
+
+    public static UUIDSet fastUUIDSetDeserialize( ObjectDataInput in ) throws IOException{
+        int size = in.readInt();
+        UUIDSet set = new UUIDSet( size );
+        return (UUIDSet) processEntries( set, in );
+    }
+
+    private static Set<UUID> processEntries( Set<UUID> set, ObjectDataInput in ) throws IOException {
+        long[] least = in.readLongArray();
+        long[] most = in.readLongArray();
+        for ( int i=0; i < set.size(); i++ ) {
+            set.add( new UUID( most[i], least[i] ) );
+        }
+        return set;
     }
 
     public static <T> Set<T> deserialize( ObjectDataInput in, IoPerformingFunction<ObjectDataInput, T> f ) throws IOException {
