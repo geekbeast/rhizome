bad284a106a3b53cfa9177422e35739b14071470
Drew <drew@kryptnostic.com>
2015-12-11 11:04:15 -0800

    Set proxies should have types

--- src/main/java/com/kryptnostic/rhizome/hazelcast/objects/SetProxy.java
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/objects/SetProxy.java
@@ -1,6 +1,9 @@
-package com.kryptnostic.rhizome.mappers;
+package com.kryptnostic.rhizome.hazelcast.objects;
 
 import java.util.Set;
 
 public interface SetProxy<T> extends Set<T> {
+
+    Class<T> getType();
+
 }
