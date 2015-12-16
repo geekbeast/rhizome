955b6855f663231cd983366151e6248d02c854f3
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-12-16 15:05:33 -0800

    Switch registry based mappers pod over to @Autowired.

--- src/main/java/com/geekbeast/rhizome/pods/RegistryBasedMappersPod.java
+++ src/main/java/com/geekbeast/rhizome/pods/RegistryBasedMappersPod.java
@@ -3,9 +3,7 @@ package com.geekbeast.rhizome.pods;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 
-import javax.inject.Inject;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.collect.Maps;
@@ -13,12 +11,13 @@ import com.kryptnostic.rhizome.mappers.KeyMapper;
 import com.kryptnostic.rhizome.mappers.SelfRegisteringKeyMapper;
 import com.kryptnostic.rhizome.mappers.SelfRegisteringValueMapper;
 import com.kryptnostic.rhizome.mappers.ValueMapper;
-import com.kryptnostic.rhizome.mapstores.MappingException;
 
 @Configuration
 public class RegistryBasedMappersPod {
-    private static final ConcurrentMap<Class<?>, SelfRegisteringKeyMapper<?>>   keyMapperRegistry   = Maps.newConcurrentMap();
-    private static final ConcurrentMap<Class<?>, SelfRegisteringValueMapper<?>> valueMapperRegistry = Maps.newConcurrentMap();
+    private static final ConcurrentMap<Class<?>, SelfRegisteringKeyMapper<?>>   keyMapperRegistry   = Maps
+                                                                                                            .newConcurrentMap();
+    private static final ConcurrentMap<Class<?>, SelfRegisteringValueMapper<?>> valueMapperRegistry = Maps
+                                                                                                            .newConcurrentMap();
 
     public static KeyMapper<?> getKeyMapper( Class<?> clazz ) {
         return keyMapperRegistry.get( clazz );
@@ -28,60 +27,19 @@ public class RegistryBasedMappersPod {
         return valueMapperRegistry.get( clazz );
     }
 
-    @Inject
+    @Autowired(
+        required = false )
     public void registerValueMappers( Set<SelfRegisteringValueMapper<?>> valueMappers ) {
         for ( SelfRegisteringValueMapper<?> mapper : valueMappers ) {
             valueMapperRegistry.put( mapper.getClazz(), mapper );
         }
     }
 
-    @Inject
+    @Autowired(
+        required = false )
     public void registerKeyMappers( Set<SelfRegisteringKeyMapper<?>> keyMappers ) {
         for ( SelfRegisteringKeyMapper<?> mapper : keyMappers ) {
             keyMapperRegistry.put( mapper.getClazz(), mapper );
         }
     }
-
-    @Bean
-    public SelfRegisteringKeyMapper<?> noopKM() {
-        return new SelfRegisteringKeyMapper<Void>() {
-
-            @Override
-            public String fromKey( Void key ) {
-                return null;
-            }
-
-            @Override
-            public Void toKey( String value ) {
-                return null;
-            }
-
-            @Override
-            public Class<Void> getClazz() {
-                return Void.class;
-            }
-        };
-    }
-
-    @Bean
-    public SelfRegisteringValueMapper<?> noopVM() {
-        return new SelfRegisteringValueMapper<Void>() {
-
-            @Override
-            public byte[] toBytes( Void value ) throws MappingException {
-                return null;
-            }
-
-            @Override
-            public Void fromBytes( byte[] data ) throws MappingException {
-                return null;
-            }
-
-            @Override
-            public Class<Void> getClazz() {
-                return Void.class;
-            }
-        };
-    }
-
 }
