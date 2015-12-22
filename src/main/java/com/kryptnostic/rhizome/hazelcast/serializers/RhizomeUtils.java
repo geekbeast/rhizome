ac9d25330f1aedcbff3ee0c28abd226d26287c7f
Drew <drew@kryptnostic.com>
2015-12-21 18:28:55 -0800

    add stringarray and mapStringString helpers

--- src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/RhizomeUtils.java
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/RhizomeUtils.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Maps;
 import com.google.common.primitives.Ints;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
 import com.kryptnostic.rhizome.hazelcast.objects.OrderedUUIDSet;
 import com.kryptnostic.rhizome.hazelcast.objects.UUIDSet;
 
@@ -45,6 +51,41 @@ public class RhizomeUtils {
     }
 
     public static class Streams {
+
+        public static void writeStringStringMap( ObjectDataOutput out, Map<String, String> object ) throws IOException {
+            int size = object.size();
+            Set<String> keys = object.keySet();
+            Collection<String> vals = object.values();
+            RhizomeUtils.Streams.writeStringArray( out, keys.toArray( new String[ size ] ) );
+            RhizomeUtils.Streams.writeStringArray( out, vals.toArray( new String[ size ] ) );
+        }
+
+        public static Map<String, String> readStringStringMap( ObjectDataInput in ) throws IOException {
+            String[] keys = RhizomeUtils.Streams.readStringArray( in );
+            String[] vals = RhizomeUtils.Streams.readStringArray( in );
+            Map<String, String> map = Maps.newHashMapWithExpectedSize( keys.length );
+            for ( int i = 0; i < keys.length; i++ ) {
+                map.put( keys[ i ], vals[ i ] );
+            }
+            return map;
+        }
+
+        public static void writeStringArray( ObjectDataOutput out, String[] strings ) throws IOException {
+            out.writeInt( strings.length );
+            for ( String string : strings ) {
+                out.writeUTF( string );
+            }
+        }
+
+        public static String[] readStringArray( ObjectDataInput in ) throws IOException {
+            int size = in.readInt();
+            String[] strings = new String[ size ];
+
+            for ( int i = 0; i < size; i++ ) {
+                strings[ i ] = in.readUTF();
+            }
+            return strings;
+        }
 
         public static void writeByteArray( ObjectOutput out, byte[] bytes ) throws IOException {
             out.writeInt( bytes.length );
