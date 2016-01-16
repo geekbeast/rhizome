54fb98fc5bf16fbb13aeeafed9bf18d29c675f10
Katherine <katherine@kryptnostic.com>
2016-01-15 17:22:13 -0800

    keep connection alive

--- src/main/java/com/kryptnostic/rhizome/configuration/websockets/WebSocketConfig.java
+++ src/main/java/com/kryptnostic/rhizome/configuration/websockets/WebSocketConfig.java
@@ -3,16 +3,23 @@ package com.kryptnostic.rhizome.configuration.websockets;
 import org.eclipse.jetty.websocket.api.WebSocketBehavior;
 import org.eclipse.jetty.websocket.api.WebSocketPolicy;
 import org.eclipse.jetty.websocket.server.WebSocketServerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.server.RequestUpgradeStrategy;
 import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
 import com.kryptnostic.helper.services.v1.HandshakeInterceptor;
 
 
@@ -49,6 +56,19 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	@Bean
 	public DefaultHandshakeHandler handshakeHandler() {
 		return new DefaultHandshakeHandler(upgradeStrategy());
+	}
+	
+	@Bean
+	public WebSocketStompClient client() {
+		LoggerFactory.getLogger( WebSocketConfig.class ).info("IN STOMP CLIENT CREATION");
+		WebSocketClient transport = new StandardWebSocketClient();
+		WebSocketStompClient stompClient = new WebSocketStompClient(transport);
+		stompClient.setMessageConverter(new StringMessageConverter());
+		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.afterPropertiesSet();
+        stompClient.setTaskScheduler(taskScheduler);
+        stompClient.setReceiptTimeLimit(5000);
+        return stompClient;
 	}
 
 
