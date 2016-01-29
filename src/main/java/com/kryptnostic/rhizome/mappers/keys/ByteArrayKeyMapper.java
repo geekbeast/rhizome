1dda36d1c2c8c5b6b01dcd60f529b2b3bb73b8ef
Elliott Jin <elliott@kryptnostic.com>
2016-01-29 11:30:29 -0800

    Specify charset for encoding / decoding ByteArrayKeyMapper

--- src/main/java/com/kryptnostic/rhizome/mappers/keys/ByteArrayKeyMapper.java
+++ src/main/java/com/kryptnostic/rhizome/mappers/keys/ByteArrayKeyMapper.java
@@ -1,17 +1,22 @@
 package com.kryptnostic.rhizome.mappers.keys;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import com.kryptnostic.rhizome.mappers.SelfRegisteringKeyMapper;
 
 public class ByteArrayKeyMapper implements SelfRegisteringKeyMapper<byte[]> {
 
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+
     @Override
     public String fromKey( byte[] key ) {
-        return key == null ? null : new String( key );
+        return key == null ? null : new String( key, CHARSET );
     }
 
     @Override
     public byte[] toKey( String value ) {
-        return value.getBytes();
+        return value.getBytes( CHARSET );
     }
 
     @Override
