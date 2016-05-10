282d0acfacb058096462c8312c4e413a35b6d4e1
Drew Bailey <drew@kryptnostic.com>
2016-05-10 13:50:57 -0700

    cleanup cassandra map stores and add a better default for loadAll

--- src/main/java/com/kryptnostic/rhizome/mapstores/cassandra/BaseCassandraMapStore.java
+++ src/main/java/com/kryptnostic/rhizome/mapstores/cassandra/BaseCassandraMapStore.java
@@ -3,12 +3,20 @@ package com.kryptnostic.rhizome.mapstores.cassandra;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.google.common.collect.MapMaker;
 import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MapStoreConfig;
 import com.kryptnostic.rhizome.configuration.cassandra.CassandraConfiguration;
@@ -39,6 +47,7 @@ public abstract class BaseCassandraMapStore<K, V> implements TestableSelfRegiste
     protected final String          table;
     protected final String                        keyspace;
     protected final int                           replicationFactor;
+    protected boolean                             SUPPORTS_ASYNC_LOADS      = true;
 
     public BaseCassandraMapStore(
             String table,
@@ -53,13 +62,54 @@ public abstract class BaseCassandraMapStore<K, V> implements TestableSelfRegiste
         this.session = session;
         this.mapName = mapName;
         this.replicationFactor = config.getReplicationFactor();
-
-        keyspace = config.getKeyspace();
-
+        this.keyspace = config.getKeyspace();
     }
 
     @Override
     public Map<K, V> loadAll( Collection<K> keys ) {
+        if ( !SUPPORTS_ASYNC_LOADS ) {
+            return defaultLoadAll( keys );
+        }
+        return asyncLoadAll( keys );
+    }
+
+    private Map<K, V> asyncLoadAll( Collection<K> keys ) {
+        ConcurrentMap<K, V> resultMap = new MapMaker()
+                .initialCapacity( keys.size() )
+                .concurrencyLevel( 8 )
+                .<K, V> makeMap();
+
+        CountDownLatch latch = new CountDownLatch( keys.size() );
+        for ( K key : keys ) {
+            Futures.addCallback( asyncLoad( key ), new FutureCallback<ResultSet>() {
+                @Override
+                public void onSuccess( ResultSet results ) {
+                    for ( Row row : results ) {
+                        resultMap.put( key, mapToValue( row ) );
+                    }
+                    latch.countDown();
+                }
+
+                @Override
+                public void onFailure( Throwable t ) {
+                    System.err.println( "Asynchronous cassandra queries failed" );
+                    t.printStackTrace();
+                    latch.countDown();
+                }
+            });
+        }
+
+        try {
+            latch.await();
+        } catch ( InterruptedException e ) {
+            System.err.println( "Async queries timed out." );
+            e.printStackTrace();
+        }
+
+        return resultMap;
+    }
+
+    private Map<K, V> defaultLoadAll( Collection<K> keys ) {
         // Naive implementation for now :(
         Map<K, V> result = Maps.newHashMap();
         for (K key : keys) {
@@ -70,6 +120,12 @@ public abstract class BaseCassandraMapStore<K, V> implements TestableSelfRegiste
         }
         return result;
     }
+
+    protected abstract ResultSetFuture asyncLoad( K key );
+
+    protected abstract K mapToKey( Row row );
+
+    protected abstract V mapToValue( Row row );
 
     @Override
     public void storeAll( Map<K, V> map ) {
