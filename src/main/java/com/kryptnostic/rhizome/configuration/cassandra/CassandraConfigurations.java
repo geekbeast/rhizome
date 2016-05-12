205318edde91739cd04c6e96f50e7e19879b347e
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-05-12 10:57:43 -0700

    Added support for multiple clusters and sessions.

--- src/main/java/com/kryptnostic/rhizome/configuration/cassandra/CassandraConfigurations.java
+++ src/main/java/com/kryptnostic/rhizome/configuration/cassandra/CassandraConfigurations.java
@@ -1,8 +1,21 @@
 package com.kryptnostic.rhizome.configuration.cassandra;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CassandraConfigurations extends HashMap<String, CassandraConfiguration> {
     private static final long serialVersionUID = -624423583792146800L;
+
+    public CassandraConfigurations() {
+        super();
+    }
+
+    public CassandraConfigurations( int initialSize ) {
+        super( initialSize );
+    }
+
+    public CassandraConfigurations( Map<String, CassandraConfiguration> configurations ) {
+        super( configurations );
+    }
 
 }
