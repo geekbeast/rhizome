ce08f8b7cc7a5da9a31cd2b861bfb9991c580cd3
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-08-10 19:22:38 -0700

    Added serializer utils for serializing string arrays.

---
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/StreamSerializerUtils.java
@@ -0,0 +1,28 @@
+package com.kryptnostic.rhizome.hazelcast.serializers;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+
+public final class StreamSerializerUtils {
+    private StreamSerializerUtils() {}
+
+    public static void writeStringList( ObjectDataOutput out, List<String> l ) throws IOException {
+        out.writeInt( l.size() );
+        for ( String elem : l ) {
+            out.writeUTF( elem );
+        }
+    }
+
+    public static List<String> readStringArrayList( ObjectDataInput in ) throws IOException {
+        int size = in.readInt();
+        String[] elems = new String[ size ];
+        for ( int i = 0; i < size; ++i ) {
+            elems[ i ] = in.readUTF();
+        }
+        return Arrays.asList( elems );
+    }
+}
