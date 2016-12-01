9fe324863790df5ac75decca54d824e236ad9a1e
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-12-01 01:56:47 -0800

    Fix profile handling in app server

--- src/main/java/com/kryptnostic/rhizome/core/RhizomeApplicationServer.java
+++ src/main/java/com/kryptnostic/rhizome/core/RhizomeApplicationServer.java
@@ -1,8 +1,10 @@
 package com.kryptnostic.rhizome.core;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.kryptnostic.rhizome.configuration.ConfigurationConstants.Profiles;
 import com.kryptnostic.rhizome.hazelcast.serializers.RhizomeUtils.Pods;
 import com.kryptnostic.rhizome.pods.AsyncPod;
 import com.kryptnostic.rhizome.pods.ConfigurationPod;
@@ -24,11 +26,24 @@ public class RhizomeApplicationServer {
     }
 
     public void sprout( String... activeProfiles ) {
-        for ( String activeProfile : activeProfiles ) {
-            context.getEnvironment().addActiveProfile( activeProfile );
+        boolean awsProfile = false;
+        boolean localProfile = false;
+        for ( String profile : activeProfiles ) {
+            if ( StringUtils.equals( Profiles.AWS_CONFIGURATION_PROFILE, profile ) ) {
+                awsProfile = true;
+            }
+
+            if ( StringUtils.equals( Profiles.LOCAL_CONFIGURATION_PROFILE, profile ) ) {
+                localProfile = true;
+            }
+
+            context.getEnvironment().addActiveProfile( profile );
+        }
+
+        if ( !awsProfile && !localProfile ) {
+            context.getEnvironment().addActiveProfile( Profiles.LOCAL_CONFIGURATION_PROFILE );
         }
         context.refresh();
-        context.start();
     }
 
     public void plowUnder() {
