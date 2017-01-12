32f46614ad622a44424a4b5bd0db8c900f73a0af
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-12 14:31:14 -0800

    Check if CassandraUnit is compatible

---
+++ src/test/java/com/geekbeast/rhizome/tests/bootstrap/CassandraBootstrap.java
@@ -0,0 +1,29 @@
+package com.geekbeast.rhizome.tests.bootstrap;
+
+import java.io.IOException;
+
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.thrift.transport.TTransportException;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CassandraBootstrap {
+    private static Logger logger = LoggerFactory.getLogger( CassandraBootstrap.class );
+
+    @BeforeClass
+    public static void startCassandra() throws ConfigurationException, TTransportException, IOException {
+        EmbeddedCassandraServerHelper
+                .startEmbeddedCassandra( EmbeddedCassandraServerHelper.CASSANDRA_RNDPORT_YML_FILE );
+        logger.info( "Started cassandra on port: {}", EmbeddedCassandraServerHelper.getNativeTransportPort() );
+    }
+
+    @Test
+    public void foo() throws InterruptedException {
+        Thread.sleep( 100000 );
+    }
+    
+    
+}
