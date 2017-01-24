927e656e2a3b23fb3fb2f53057868faaebdf2312
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-23 21:56:44 -0800

    added serializer stuffs

--- src/test/java/com/kryptnostic/rhizome/hazelcast/serializers/BaseSerializerTest.java
+++ src/test/java/com/kryptnostic/rhizome/hazelcast/serializers/BaseSerializerTest.java
@@ -32,7 +32,7 @@ public abstract class BaseSerializerTest<T extends StreamSerializer<D>, D> {
         return serializer;
     }
 
-    protected abstract <S extends D> S createInput();
+    protected abstract D createInput();
 
     @Test
     public void testSerializeDeserialize() throws SecurityException, IOException {
