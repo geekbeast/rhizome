0438a1a1dfc79d03c7e059e101393f65ad3477ba
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-25 06:45:17 -0700

    Cachable auth requests
    The purpose of the cache is to avoid hitting rate limiting on auth0 when requesting the same user with the same options across several tests.

---
+++ src/test/java/digital/loom/rhizome/authentication/AuthenticationTestRequestOptions.java
@@ -0,0 +1,72 @@
+package digital.loom.rhizome.authentication;
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
+ */
+public class AuthenticationTestRequestOptions {
+    private String usernameOrEmail;
+    private String password;
+    private String connection = "Test";
+    private String scope = "openid email nickname roles user_id organizations";
+
+    @Override public boolean equals( Object o ) {
+        if ( this == o )
+            return true;
+        if ( o == null || getClass() != o.getClass() )
+            return false;
+
+        AuthenticationTestRequestOptions that = (AuthenticationTestRequestOptions) o;
+
+        if ( !usernameOrEmail.equals( that.usernameOrEmail ) )
+            return false;
+        if ( !password.equals( that.password ) )
+            return false;
+        if ( !connection.equals( that.connection ) )
+            return false;
+        return scope.equals( that.scope );
+    }
+
+    @Override public int hashCode() {
+        int result = usernameOrEmail.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + connection.hashCode();
+        result = 31 * result + scope.hashCode();
+        return result;
+    }
+
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public AuthenticationTestRequestOptions setUsernameOrEmail( String usernameOrEmail ) {
+        this.usernameOrEmail = usernameOrEmail;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public AuthenticationTestRequestOptions setPassword( String password ) {
+        this.password = password;
+        return this;
+    }
+
+    public String getConnection() {
+        return connection;
+    }
+
+    public AuthenticationTestRequestOptions setConnection( String connection ) {
+        this.connection = connection;
+        return this;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public AuthenticationTestRequestOptions setScope( String scope ) {
+        this.scope = scope;
+        return this;
+    }
+}
