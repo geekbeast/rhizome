57f83d456af42aaeffe808ca094b70a1709c9c79
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-11-06 11:26:44 -0800

    Stream loading of data

--- src/main/java/com/openlattice/postgres/mapstores/AbstractBasePostgresMapstore.java
+++ src/main/java/com/openlattice/postgres/mapstores/AbstractBasePostgresMapstore.java
@@ -22,7 +22,6 @@ package com.openlattice.postgres.mapstores;
 
 import com.dataloom.streams.StreamUtil;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MapStoreConfig;
 import com.kryptnostic.rhizome.mapstores.TestableSelfRegisteringMapStore;
@@ -31,7 +30,6 @@ import com.openlattice.postgres.KeyIterator;
 import com.openlattice.postgres.PostgresColumnDefinition;
 import com.openlattice.postgres.PostgresTableDefinition;
 import com.zaxxer.hikari.HikariDataSource;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -44,8 +42,6 @@ import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -181,14 +177,13 @@ public abstract class AbstractBasePostgresMapstore<K, V> implements TestableSelf
 
     @Override public Iterable<K> loadAllKeys() {
         logger.info( "Starting load all keys for Edge Mapstore" );
-        Stream<K> keys;
         try ( Connection connection = hds.getConnection(); Statement stmt = connection.createStatement() ) {
             connection.setAutoCommit( false );
             stmt.setFetchSize( 50000 );
             final ResultSet rs = stmt.executeQuery( selectAllKeysQuery );
             return StreamUtil
-                    .stream( Lists.newArrayList( new KeyIterator<K>( rs,
-                            new CountdownConnectionCloser( connection, 1 ), this::mapToKey ) ) )
+                    .stream( () -> new KeyIterator<K>( rs,
+                            new CountdownConnectionCloser( connection, 1 ), this::mapToKey ) )
                     .peek( key -> logger.debug( "Key to load: {}", key ) )
                     ::iterator;
         } catch ( SQLException e ) {
