299c1f01bdb0b286f635b3f821309fef3aa79ef2
Matthew Tamayo-Rios <matthew@openlattice.com>
2017-11-18 14:25:44 -0800

    Statically load auth0 configuration

--- src/main/java/digital/loom/rhizome/authentication/LocalAuth0Pod.java
+++ src/main/java/digital/loom/rhizome/authentication/LocalAuth0Pod.java
@@ -1,18 +1,15 @@
 package digital.loom.rhizome.authentication;
 
-import java.io.IOException;
-
-import javax.inject.Inject;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 import com.auth0.Auth0;
 import com.kryptnostic.rhizome.configuration.ConfigurationConstants.Profiles;
 import com.kryptnostic.rhizome.configuration.service.ConfigurationService;
-
+import com.openlattice.ResourceConfigurationLoader;
 import digital.loom.rhizome.configuration.auth0.Auth0Configuration;
+import java.io.IOException;
+import javax.inject.Inject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile( Profiles.LOCAL_CONFIGURATION_PROFILE )
@@ -22,7 +19,7 @@ public class LocalAuth0Pod {
 
     @Bean
     public Auth0Configuration auth0Configuration() throws IOException {
-        return configService.getConfiguration( Auth0Configuration.class );
+        return ResourceConfigurationLoader.loadConfiguration( Auth0Configuration.class );
     }
 
     @Bean
@@ -30,3 +27,4 @@ public class LocalAuth0Pod {
         return new Auth0( auth0Configuration().getClientId(), auth0Configuration().getDomain() );
     }
 }
+
