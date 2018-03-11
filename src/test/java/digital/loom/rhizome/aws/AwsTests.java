8fd297a1a9236c92ea028e7a6539cabaf627f546
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-03-11 03:43:33 -0700

    Woops

--- src/test/java/digital/loom/rhizome/aws/AwsTests.java
+++ src/test/java/digital/loom/rhizome/aws/AwsTests.java
@@ -1,5 +1,7 @@
 package digital.loom.rhizome.aws;
 
+import com.kryptnostic.rhizome.configuration.amazon.AmazonConfiguration;
+import com.kryptnostic.rhizome.configuration.amazon.AwsLaunchConfiguration;
 import com.openlattice.ResourceConfigurationLoader;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,6 +13,6 @@ public class AwsTests {
 
     @Test
     public void testLoadAmazonLaunchConfiguration() {
-        Assert.assertNotNull( ResourceConfigurationLoader.loadConfigurationFromResource( "aws.yaml", AmazonLaunchConfiguration.class ) );
+        Assert.assertNotNull( ResourceConfigurationLoader.loadConfigurationFromResource( "aws.yaml", AwsLaunchConfiguration.class ) );
     }
 }
