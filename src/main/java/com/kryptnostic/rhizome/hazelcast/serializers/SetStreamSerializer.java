41eba3e6d8576173729d22d3e2f16275be9a70a7
you-leee <julia@openlattice.com>
2019-06-24 18:15:38 -0700

    LATTICE-2082 create general collection stream serializer

--- src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/SetStreamSerializer.java
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/serializers/SetStreamSerializer.java
@@ -1,49 +1,23 @@
 package com.kryptnostic.rhizome.hazelcast.serializers;
 
+import com.hazelcast.nio.ObjectDataInput;
+
 import java.io.IOException;
 import java.util.Set;
 
-import com.hazelcast.nio.ObjectDataInput;
-import com.hazelcast.nio.ObjectDataOutput;
-import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer;
-
-public abstract class SetStreamSerializer<T extends Set<E>, E> implements SelfRegisteringStreamSerializer<T> {
-    private Class<T> clazz;
+public abstract class SetStreamSerializer<T extends Set<E>, E> extends CollectionStreamSerializer<T, E> {
 
     protected SetStreamSerializer( Class<T> clazz ) {
-        this.clazz = clazz;
-    }
-
-    @Override
-    public void write( ObjectDataOutput out, T object ) throws IOException {
-        out.writeInt( object.size() );
-        for ( E element : object ) {
-            writeSingleElement( out, element );
-        }
+        super( clazz );
     }
 
     @Override
     public T read( ObjectDataInput in ) throws IOException {
-        int size = in.readInt();
-        T obj = newInstanceWithExpectedSize( size );
-        for ( int i = 0; i < size; ++i ) {
+        var size = in.readInt();
+        final var obj = newInstanceWithExpectedSize( size );
+        for ( int i = 0; i < size; i++ ) {
             obj.add( readSingleElement( in ) );
         }
         return obj;
     }
-
-    protected abstract T newInstanceWithExpectedSize( int size );
-
-    protected abstract E readSingleElement( ObjectDataInput in ) throws IOException;
-
-    protected abstract void writeSingleElement( ObjectDataOutput out, E element ) throws IOException;
-
-    @Override
-    public void destroy() {}
-
-    @Override
-    public Class<T> getClazz() {
-        return clazz;
-    }
-
 }
