baf5403d1e2dca06898bb9446b78922cceee4b9c
Katherine Bernstein <katherine@openlattice.com>
2019-07-05 17:02:10 -0700

    add equals and hash code to DelegatedIntList

--- src/main/kotlin/com/geekbeast/rhizome/hazelcast/DelegatedIntList.kt
+++ src/main/kotlin/com/geekbeast/rhizome/hazelcast/DelegatedIntList.kt
@@ -4,4 +4,14 @@ package com.geekbeast.rhizome.hazelcast
  * A quick and dirty delegation wrapper for hazelast serialization of int lists.
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
-class DelegatedIntList(list: List<Int>) : List<Int> by list
+class DelegatedIntList(list: List<Int>) : List<Int> by list {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
