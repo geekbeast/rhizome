900d135286e48b2b367fdbfde7f7f25d214e40ef
you-leee <julia@openlattice.com>
2020-02-04 15:12:23 -0800

    LATTICE-2392 purge

--- src/main/java/com/kryptnostic/rhizome/cassandra/CassandraMapper.java
+++
@@ -1,8 +0,0 @@
-package com.kryptnostic.rhizome.cassandra;
-
-import com.kryptnostic.rhizome.mappers.ValueMapper;
-
-public interface CassandraMapper<V> extends ValueMapper<V> {
-    V map( String data );
-    String asString( V input );
-}
--- src/main/java/com/kryptnostic/rhizome/mappers/CompoundKeyMapper.java
+++
@@ -1,5 +0,0 @@
-package com.kryptnostic.rhizome.mappers;
-
-public interface CompoundKeyMapper<K> {
-    Object getKeyPart( int i );
-}
