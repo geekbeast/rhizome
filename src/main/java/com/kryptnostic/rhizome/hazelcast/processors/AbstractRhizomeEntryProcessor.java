5ef9cc42afd7b9b1872b4c99208e3e8822ee2787
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-04-09 19:28:35 -0700

    Fix stuff for hazelcast 4.0

--- src/main/java/com/kryptnostic/rhizome/hazelcast/processors/AbstractRhizomeEntryProcessor.java
+++ src/main/java/com/kryptnostic/rhizome/hazelcast/processors/AbstractRhizomeEntryProcessor.java
@@ -1,17 +1,14 @@
 package com.kryptnostic.rhizome.hazelcast.processors;
 
-import java.util.Map;
-import java.util.Map.Entry;
-
-import com.hazelcast.map.EntryBackupProcessor;
+import com.hazelcast.cache.BackupAwareEntryProcessor;
 import com.hazelcast.map.EntryProcessor;
-
+import java.util.Map;
 import javax.annotation.Nonnull;
 
 public abstract class AbstractRhizomeEntryProcessor<K, V, R>
-        implements EntryProcessor<K, V>, EntryBackupProcessor<K, V> {
-    private static final long serialVersionUID = 5060655249179605949L;
-    private final boolean     applyOnBackup;
+        implements EntryProcessor<K, V, R>, BackupAwareEntryProcessor<K, V, R> {
+    private static final long    serialVersionUID = 5060655249179605949L;
+    private final        boolean applyOnBackup;
 
     /**
      * Creates an EntryProcessor that applies the {@link #process(java.util.Map.Entry)} to primary and backups.
@@ -20,24 +17,19 @@ public abstract class AbstractRhizomeEntryProcessor<K, V, R>
         this( true );
     }
 
-    @Override
-    public abstract R process( @Nonnull Map.Entry<K, V> entry );
-
     public AbstractRhizomeEntryProcessor( boolean applyOnBackup ) {
         this.applyOnBackup = applyOnBackup;
     }
 
     @Override
-    public final EntryBackupProcessor<K, V> getBackupProcessor() {
+    public abstract R process( @Nonnull Map.Entry<K, V> entry );
+
+    @Override
+    public final EntryProcessor<K, V, R> getBackupProcessor() {
         if ( applyOnBackup ) {
             return this;
         }
         return null;
-    }
-
-    @Override
-    public void processBackup( Entry<K, V> entry ) {
-        process( entry );
     }
 
 }
