cf794667978a84880e94bce31ccea53ee53890a6
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-06-16 20:06:37 -0700

    Added commonly used missing functions.

--- src/test/kotlin/com/geekbeast/rhizome/hazelcast/HazelcastQueueInMemoryMock.kt
+++ src/test/kotlin/com/geekbeast/rhizome/hazelcast/HazelcastQueueInMemoryMock.kt
@@ -16,30 +16,30 @@ import java.util.concurrent.TimeUnit
  * @param valueClass A reference to the class of the value used for the map
  * @return A mocked IMap backed by a concurrent map.
  */
-fun <V> mockHazelcastQueue(valueClass: Class<V>) : IQueue<V> {
-    val mock =  Mockito.mock<IQueue<*>>( IQueue::class.java) as IQueue<V>
+fun <V> mockHazelcastQueue(valueClass: Class<V>): IQueue<V> {
+    val mock = Mockito.mock<IQueue<*>>(IQueue::class.java) as IQueue<V>
     val backingQueue = Queues.newArrayBlockingQueue<V>(10000)
 
     Mockito.`when`(mock.put(any(valueClass))).thenAnswer {
-        val v  = it.arguments[0] as V
-        backingQueue.put(v )
+        val v = it.arguments[0] as V
+        backingQueue.put(v)
     }
 
     Mockito.`when`(mock.add(any(valueClass))).thenAnswer {
-        val v  = it.arguments[0] as V
-        backingQueue.add( v )
+        val v = it.arguments[0] as V
+        backingQueue.add(v)
     }
 
     Mockito.`when`(mock.offer(any(valueClass))).thenAnswer {
-        val v  = it.arguments[0] as V
-        backingQueue.offer( v )
+        val v = it.arguments[0] as V
+        backingQueue.offer(v)
     }
 
-    Mockito.`when`(mock.offer(any(valueClass),any(Long::class.java),any(TimeUnit::class.java))).thenAnswer {
-        val v  = it.arguments[0] as V
+    Mockito.`when`(mock.offer(any(valueClass), any(Long::class.java), any(TimeUnit::class.java))).thenAnswer {
+        val v = it.arguments[0] as V
         val timeout = it.arguments[1] as Long
         val timeUnit = it.arguments[2] as TimeUnit
-        backingQueue.offer( v, timeout, timeUnit )
+        backingQueue.offer(v, timeout, timeUnit)
     }
 
     Mockito.`when`(mock.take()).thenAnswer {
@@ -50,11 +50,14 @@ fun <V> mockHazelcastQueue(valueClass: Class<V>) : IQueue<V> {
         backingQueue.poll()
     }
 
-    Mockito.`when`(mock.poll(any(Long::class.java),any(TimeUnit::class.java))).thenAnswer {
+    Mockito.`when`(mock.poll(any(Long::class.java), any(TimeUnit::class.java))).thenAnswer {
         val timeout = it.arguments[0] as Long
         val timeUnit = it.arguments[1] as TimeUnit
-        backingQueue.poll( timeout, timeUnit )
+        backingQueue.poll(timeout, timeUnit)
     }
-    
+
+    Mockito.`when`(mock.clear()).thenAnswer { backingQueue.clear() }
+    Mockito.`when`(mock.size).thenAnswer { backingQueue.size }
+
     return mock
 }
