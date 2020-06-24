184740815adc06f3d1c8115c88445d63756db6b7
Drew <drew@openlattice.com>
2020-06-24 18:06:48 -0400

    overload equals

--- src/main/kotlin/com/openlattice/rhizome/DelegatedIntSet.kt
+++ src/main/kotlin/com/openlattice/rhizome/DelegatedIntSet.kt
@@ -3,4 +3,11 @@ package com.openlattice.rhizome
 /**
  * @author Drew Bailey &lt;drew@openlattice.com&gt;
  */
-class DelegatedIntSet( ints: Set<Int> ): Set<Int> by ints
+class DelegatedIntSet( ints: Set<Int> ): Set<Int> by ints {
+    override fun equals(other: Any?): Boolean {
+        if (other === this) return true
+        if (!(other is DelegatedIntSet)) return false
+        if (other.size != size) return false
+        return containsAll(other)
+    }
+}
