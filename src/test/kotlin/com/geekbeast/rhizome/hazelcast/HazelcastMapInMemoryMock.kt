6088790fb438a2db49ae20f12c2d5ecbfd2ef02c
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-06-24 22:57:37 -0700

    A bit of overkill for mocks + int set fix.

--- src/test/kotlin/com/geekbeast/rhizome/hazelcast/HazelcastMapInMemoryMock.kt
+++ src/test/kotlin/com/geekbeast/rhizome/hazelcast/HazelcastMapInMemoryMock.kt
@@ -5,15 +5,13 @@ import com.hazelcast.core.IMap
 import com.hazelcast.internal.serialization.InternalSerializationService
 import com.hazelcast.internal.serialization.impl.DefaultSerializationServiceBuilder
 import com.hazelcast.internal.serialization.impl.SerializationServiceV1
-import com.hazelcast.nio.serialization.Data
-import com.hazelcast.nio.serialization.StreamSerializer
 import com.hazelcast.query.Predicate
 import com.hazelcast.query.impl.CachedQueryEntry
-import com.hazelcast.query.impl.QueryableEntry
 import com.hazelcast.query.impl.getters.Extractors
 import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer
-import org.mockito.Matchers.any
+import org.mockito.Matchers.*
 import org.mockito.Mockito
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 
 
@@ -38,7 +36,10 @@ fun <K, V> mockHazelcastMap(
         extractors: Extractors = Extractors.newBuilder(ss).setMapAttributeConfigs(listOf()).setClassLoader(keyClass.classLoader).build()
 ): IMap<K, V> {
     val mock = Mockito.mock<IMap<*, *>>(IMap::class.java) as IMap<K, V>
-    val backingMap = Maps.newConcurrentMap<K, V>() as MutableMap<K,V>
+    val backingMap = Maps.newConcurrentMap<K, V>()
+    val ttlMap = Maps.newConcurrentMap<K, Long>()
+    val idleMap = Maps.newConcurrentMap<K, IdleInfo>()
+
 //    val ssMap = streamSerializers.associateBy { it.clazz }
 //    val keyStreamSerializer = getSerializers(ssMap, keyClass)
 //    val valueStreamSerializer = getSerializers(ssMap, valueClass)
@@ -66,18 +67,60 @@ fun <K, V> mockHazelcastMap(
     Mockito.`when`(mock.putIfAbsent(any(keyClass), any(valueClass))).thenAnswer {
         val k = it.arguments[0] as K
         val v = it.arguments[1] as V
+        handleExpiration(k, backingMap, ttlMap)
+        handleIdleness(k, backingMap, ttlMap, idleMap)
+        backingMap.putIfAbsent(k, v)
+    }
+
+    Mockito.`when`(
+            mock.putIfAbsent(
+                    any(keyClass),
+                    any(valueClass),
+                    anyLong(),
+                    any(TimeUnit::class.java),
+                    anyLong(),
+                    any(TimeUnit::class.java)
+            )
+    ).thenAnswer {
+        val k = it.arguments[0] as K
+        val v = it.arguments[1] as V
+
+        val ttlUnit = it.arguments[3] as TimeUnit
+        val ttl = ttlUnit.toNanos(it.arguments[2] as Long)
+
+        val maxIdleTimeUnit = it.arguments[5] as TimeUnit
+        val maxIdleTime = maxIdleTimeUnit.toNanos(it.arguments[4] as Long)
+
+        val currentNanos = System.nanoTime()
+
+        //Only start tracking if ttl or maxIdleTime are positive
+        if (ttl > 0) ttlMap[k] = currentNanos + ttl
+        if (maxIdleTime > 0) idleMap[k] = IdleInfo(maxIdleTime, currentNanos + maxIdleTime)
+
         backingMap.putIfAbsent(k, v)
     }
 
     Mockito.`when`(mock.set(any(keyClass), any(valueClass))).thenAnswer {
         val k = it.arguments[0] as K
         val v = it.arguments[1] as V
+        handleIdleness(k, backingMap, ttlMap, idleMap)
         backingMap[k] = v
         Unit
     }
 
+    Mockito.`when`(mock.getAll(anySetOf(keyClass))).thenAnswer { invocation ->
+        val keys = invocation.arguments[0] as Set<K>
+        keys.forEach { k ->
+            handleExpiration(k, backingMap, ttlMap)
+            handleIdleness(k, backingMap, ttlMap, idleMap)
+        }
+        keys.associateWith { backingMap[it] }
+    }
+
     Mockito.`when`(mock.get(any(keyClass))).thenAnswer {
         val k = it.arguments[0] as K
+        handleExpiration(k, backingMap, ttlMap)
+        handleIdleness(k, backingMap, ttlMap, idleMap)
         backingMap[k]
     }
 
@@ -89,11 +132,15 @@ fun <K, V> mockHazelcastMap(
 
     Mockito.`when`(mock.remove(any(keyClass))).thenAnswer {
         val k = it.arguments[0] as K
+        handleExpiration(k, backingMap, ttlMap)
+        handleIdleness(k, backingMap, ttlMap, idleMap)
         backingMap.remove(k)
     }
 
     Mockito.`when`(mock.entrySet(any(Predicate::class.java))).thenAnswer { invocation ->
         val p = invocation.arguments[0] as Predicate<K, V>
+        handleExpiration(backingMap, ttlMap)
+        handleIdleness(backingMap, ttlMap, idleMap)
         backingMap.asSequence()
                 .map { CachedQueryEntry<K, V>(ss, ss.toData(it.component1()), it.component2(), extractors) }
                 .filter(p::apply)
@@ -102,8 +149,10 @@ fun <K, V> mockHazelcastMap(
 
     Mockito.`when`(mock.keySet(any(Predicate::class.java))).thenAnswer { it ->
         val p = it.arguments[0] as Predicate<K, V>
+        handleExpiration(backingMap, ttlMap)
+        handleIdleness(backingMap, ttlMap, idleMap)
         backingMap.asSequence()
-                .map{ CachedQueryEntry<K, V>(ss,ss.toData(it.component1()),it.component2(),extractors) }
+                .map { CachedQueryEntry<K, V>(ss, ss.toData(it.component1()), it.component2(), extractors) }
                 .filter(p::apply)
                 .map { entry -> entry.component1() }
                 .toSet()
@@ -111,19 +160,21 @@ fun <K, V> mockHazelcastMap(
 
     Mockito.`when`(mock.values(any(Predicate::class.java))).thenAnswer { it ->
         val p = it.arguments[0] as Predicate<K, V>
+        handleExpiration(backingMap, ttlMap)
+        handleIdleness(backingMap, ttlMap, idleMap)
         backingMap.asSequence()
-                .map{ CachedQueryEntry<K, V>(ss,ss.toData(it.component1()),it.component2(),extractors) }
+                .map { CachedQueryEntry<K, V>(ss, ss.toData(it.component1()), it.component2(), extractors) }
                 .filter(p::apply)
                 .map { entry -> entry.component1() }
                 .toList()
     }
 
-    Mockito.`when`(mock.entries).thenAnswer{ backingMap.entries }
-    Mockito.`when`(mock.keys).thenAnswer{ backingMap.keys }
-    Mockito.`when`(mock.values).thenAnswer{ backingMap.values }
-    Mockito.`when`(mock.size).thenAnswer{ backingMap.size }
+    Mockito.`when`(mock.entries).thenAnswer { backingMap.entries }
+    Mockito.`when`(mock.keys).thenAnswer { backingMap.keys }
+    Mockito.`when`(mock.values).thenAnswer { backingMap.values }
+    Mockito.`when`(mock.size).thenAnswer { backingMap.size }
     Mockito.`when`(mock.count()).thenAnswer { backingMap.count() }
-    Mockito.`when`(mock.clear()).thenAnswer{ backingMap.clear() }
+    Mockito.`when`(mock.clear()).thenAnswer { backingMap.clear() }
 
     return mock
 }
@@ -139,3 +190,64 @@ private fun getSerializers(
 ) = streamSerializers[clazz]
         ?: streamSerializers.getValue(streamSerializers.keys.first { it.isAssignableFrom(clazz) })
 
+private fun <K, V> handleExpiration(backingMap: MutableMap<K, V>, expirationMap: MutableMap<K, Long>) {
+    backingMap.keys -= expirationMap.filterValues { it <= System.nanoTime() }.keys
+}
+
+private fun <K, V> handleIdleness(
+        k: K,
+        backingMap: MutableMap<K, V>,
+        ttlMap: MutableMap<K, Long>,
+        idleMap: MutableMap<K, IdleInfo>
+) {
+    val idleInfo = idleMap[k]
+    if (idleInfo != null) {
+        if (idleInfo.expirationNanos > System.nanoTime()) {
+            //Mark data as not idle.
+            idleInfo.markNotIdle()
+        } else {
+            //Expire data from the maps
+            backingMap.remove(k)
+            idleMap.remove(k)
+            ttlMap.remove(k)
+        }
+    }
+}
+
+private fun <K, V> handleIdleness(
+        backingMap: MutableMap<K, V>,
+        ttlMap: MutableMap<K, Long>,
+        idleMap: MutableMap<K, IdleInfo>
+) {
+    idleMap.forEach { (k, idleInfo) ->
+        val idleInfo = idleMap[k]
+        if (idleInfo != null) {
+            if (idleInfo.expirationNanos > System.nanoTime()) {
+                //Mark data as not idle.
+                idleInfo.markNotIdle()
+            } else {
+                //Expire data from the maps
+                backingMap.remove(k)
+                idleMap.remove(k)
+                ttlMap.remove(k)
+            }
+        }
+    }
+}
+
+private fun <K, V> handleExpiration(k: K, backingMap: MutableMap<K, V>, ttlMap: MutableMap<K, Long>) {
+    val expiration = ttlMap[k]
+    if (expiration != null && expiration <= System.nanoTime()) {
+        backingMap.remove(k)
+        ttlMap.remove(k)
+    }
+}
+
+/**
+ * Not meant to be used externally.
+ */
+private data class IdleInfo(val maxIdleNanos: Long, var expirationNanos: Long) {
+    fun markNotIdle() {
+        expirationNanos = System.nanoTime() + maxIdleNanos
+    }
+}
---
+++ src/test/kotlin/com/geekbeast/rhizome/hazelcast/HazelcastTestUtil.kt
@@ -0,0 +1,30 @@
+package com.geekbeast.rhizome.hazelcast
+
+import com.hazelcast.config.*
+import com.hazelcast.core.Hazelcast
+import com.hazelcast.core.HazelcastInstance
+import com.kryptnostic.rhizome.pods.hazelcast.SelfRegisteringStreamSerializer
+import org.apache.commons.lang3.RandomStringUtils
+
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+
+fun startHazelcastInstance(
+        name: String,
+        group: String,
+        streamSerializers: Set<SelfRegisteringStreamSerializer<*>>
+): HazelcastInstance {
+    val config = Config(name)
+    config.groupConfig = GroupConfig(group, RandomStringUtils.random(20))
+    config.serializationConfig = SerializationConfig()
+    streamSerializers.forEach {
+        config.serializationConfig.addSerializerConfig(
+                SerializerConfig()
+                        .setTypeClass(it.clazz)
+                        .setImplementation(it)
+        )
+    }
+    return Hazelcast.newHazelcastInstance(config)
+}
