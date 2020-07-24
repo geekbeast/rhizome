49c4ffc131adfc1506de45d59a3fa8d8bcd8d494
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-07-24 00:26:02 -0700

    Initial plumbing for distributed job service.

---
+++ src/main/kotlin/com/geekbeast/rhizome/jobs/HazelcastJobService.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2020. OpenLattice, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ *
+ */
+
+package com.geekbeast.rhizome.jobs
+
+import com.hazelcast.core.HazelcastInstance
+import java.util.*
+
+const val JOBS_MAP = "_rhizome_jobs_"
+/**
+ *
+ * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
+ */
+class HazelcastJobService(hazelcastInstance: HazelcastInstance) {
+    protected val jobs = hazelcastInstance.getMap<UUID, DistributedJob<*>>(JOBS_MAP)
+}
