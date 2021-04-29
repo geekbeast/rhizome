48cf9e2dbc292e36abef8b69a93c57133d389ef3
Drew <drew@openlattice.com>
2021-04-29 19:50:27 -0400

    dont use cl

--- src/main/java/com/kryptnostic/rhizome/pods/KubernetesConfigurationPod.kt
+++ src/main/java/com/kryptnostic/rhizome/pods/KubernetesConfigurationPod.kt
@@ -3,11 +3,11 @@ package com.kryptnostic.rhizome.pods
 import com.kryptnostic.rhizome.configuration.ConfigurationConstants
 import com.kryptnostic.rhizome.configuration.RhizomeConfiguration
 import com.kryptnostic.rhizome.configuration.jetty.JettyConfiguration
+import com.openlattice.ResourceConfigurationLoader
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
-import javax.inject.Inject
 
 /**
  * Used to load configuration from files mounted in the container
@@ -15,20 +15,17 @@ import javax.inject.Inject
  * @author Drew Bailey (drew@openlattice.com)
  */
 @Configuration
-@Import(ConfigurationLoaderPod::class)
 @Profile(ConfigurationConstants.Profiles.KUBERNETES_CONFIGURATION_PROFILE)
 class KubernetesConfigurationPod {
-
-    @Inject
-    private lateinit var configurationLoader: ConfigurationLoader
+    private val logger = LoggerFactory.getLogger(KubernetesConfigurationPod::class.java)
 
     @Bean
     fun rhizomeConfiguration(): RhizomeConfiguration {
-        return configurationLoader.logAndLoad("rhizome", RhizomeConfiguration::class.java)
+        return ResourceConfigurationLoader.loadConfigurationFromFile("/etc/openlattice", RhizomeConfiguration::class.java)
     }
 
     @Bean
     fun jettyConfiguration(): JettyConfiguration {
-        return configurationLoader.logAndLoad("jetty", JettyConfiguration::class.java)
+        return ResourceConfigurationLoader.loadConfigurationFromFile("/etc/openlattice", JettyConfiguration::class.java)
     }
 }
