c898fc92826c5be28b934c2f6cff8f439f8d25a4
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-22 02:27:13 -0700

    Fix auth0 test.

--- src/test/kotlin/com/geekbeast/auth0/AwsAuth0TokenProviderTest.kt
+++ src/test/kotlin/com/geekbeast/auth0/AwsAuth0TokenProviderTest.kt
@@ -3,6 +3,7 @@ package com.geekbeast.auth0
 import com.auth0.client.auth.AuthAPI
 import com.auth0.json.auth.TokenHolder
 import com.auth0.net.AuthRequest
+import com.auth0.net.TokenRequest
 import com.geekbeast.authentication.Auth0Configuration
 import org.apache.commons.lang3.RandomStringUtils
 import org.junit.Assert
@@ -17,18 +18,15 @@ import java.util.concurrent.Executors
  * @author Matthew Tamayo-Rios &lt;matthew@openlattice.com&gt;
  */
 class AwsAuth0TokenProviderTest {
-    @Test(timeout = 2000)
+    @Test(timeout = 3000)
     // timeout means deadlock in something
     fun testMemoization() {
         val managementApiUrl =  RandomStringUtils.random(10)
         val audience = RandomStringUtils.random(10)
         val auth0Api = Mockito.mock(AuthAPI::class.java)
-        val authRequest = Mockito.mock(AuthRequest::class.java)
-        val tokenHolder = Mockito.mock(TokenHolder::class.java)
+        val authRequest = Mockito.mock(TokenRequest::class.java)
 
-        Mockito.`when`(tokenHolder.accessToken).then { RandomStringUtils.random(10) }
-        Mockito.`when`(tokenHolder.expiresIn).thenReturn(1 )
-        Mockito.`when`(authRequest.execute()).thenReturn(tokenHolder)
+        Mockito.`when`(authRequest.execute()).then { tokenHolder() }
         Mockito.`when`(auth0Api.requestToken(managementApiUrl)).thenReturn(authRequest)
         val exec = Executors.newCachedThreadPool()
         // construct the token provider on another thread in case there's a bug.
@@ -48,10 +46,20 @@ class AwsAuth0TokenProviderTest {
 
         val v1 = tokenProvider.token
         val v2 = tokenProvider.token
-        Thread.sleep(800)
+        Thread.sleep(2100)
         val v3 = tokenProvider.token
 
         Assert.assertTrue( v1==v2 )
         Assert.assertTrue( v1!=v3 )
     }
+
+    fun tokenHolder() : TokenHolder =TokenHolder(
+            RandomStringUtils.randomAlphanumeric(10),
+            "",
+            "",
+            "",
+            2,
+            "",
+            Date(Date().toInstant().plusSeconds(2).toEpochMilli())
+    )
 }
