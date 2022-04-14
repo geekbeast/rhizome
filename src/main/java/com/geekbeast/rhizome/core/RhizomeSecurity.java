6fa03c36632a87979ea37c980f61ecf4f5717c90
Matthew Tamayo <matthew@getmethodic.com>
2022-04-14 00:38:05 -0700

    Just make sure.

--- src/main/java/com/geekbeast/rhizome/core/RhizomeSecurity.java
+++ src/main/java/com/geekbeast/rhizome/core/RhizomeSecurity.java
@@ -3,13 +3,14 @@ package com.geekbeast.rhizome.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  * This class registers the Spring Security Filter chain with all servlet contexts. Do not remove.
  *
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  */
-public class RhizomeSecurity extends AbstractSecurityWebApplicationInitializer {
+public class RhizomeSecurity extends AbstractSecurityWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger logger = LoggerFactory.getLogger( RhizomeSecurity.class );
     /* No-Op */
 
