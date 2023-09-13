c172adfdd74d4218a4405c3777c58f70ac417943
Matthew Tamayo-Rios <matthew@getmethodic.com>
2023-09-13 00:15:34 -1000

    Fix formatting.

--- src/main/kotlin/com/geekbeast/rhizome/KotlinDelegatedStringSet.kt
+++ src/main/kotlin/com/geekbeast/rhizome/KotlinDelegatedStringSet.kt
@@ -3,4 +3,13 @@ package com.geekbeast.rhizome
 /**
  * @author Drew Bailey &lt;drew@openlattice.com&gt;
  */
-class KotlinDelegatedStringSet( strings: Set<String> ): Set<String> by strings
+class KotlinDelegatedStringSet(strings: Set<String>) : Set<String> by strings {
+    override fun equals(other: Any?): Boolean {
+
+        return if (other !is Set<*> ) {
+            false
+        } else {
+            this.size == other.size && this.containsAll(other)
+        }
+    }
+}
